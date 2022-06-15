de2c1914a9908e2946d5ac03a2c3ae70630a86b2
soitkarshubham <sssoitkar@gmail.com>
2022-06-15 11:33:47 +0530

    Added Registration Login Logout functionality

--- twitter/users/urls.py
+++ twitter/users/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     path('register/',views.registeruser, name = 'register'),
+    path('profile/',views.profile, name = 'update-profile'),
+    path('login/',views.loginview, name = 'login'),
+    path('logout/',views.logoutview, name = 'logout')
 ]
--- twitter/users/views.py
+++ twitter/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import Profile
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def registeruser(request):
     '''registers user'''
@@ -9,24 +10,38 @@ def registeruser(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}.')
+            #messages.success(request, f'Account created for {username}.')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+def loginview(request):
+    if request.method == 'POST':
+        un = request.POST.get('un')
+        ps = request.POST.get('ps')
+        user = authenticate(username = un, password = ps)
+        if user is not None:
+            login(request, user)
+            return redirect('update-profile')
+    return render(request,'users/login.html')
+
+def logoutview(request):
+    logout(request)
+    return redirect ('login')
+
 def profile(request):
     if request.method == 'POST':
         uform = UserUpdateForm(request.POST, instance=request.user)
-        pform = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        pform = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
 
         if uform.is_valid() and pform.is_valid():
             uform.save()
             pform.save()
-            messages.success(request, f'Account has been updated.')
-            return redirect('profile')
+            #messages.success(request, f'Account has been updated.')
+            return redirect('update-profile')
     else:
         uform = UserUpdateForm(instance=request.user)
-        pform = ProfileUpdateForm(instance=request.user.profile)
+        pform = ProfileUpdateForm(instance=request.user)
 
     return render(request, 'users/profile.html', {'uform': uform, 'pform': pform})
